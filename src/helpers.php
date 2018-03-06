10bf1958dc11406dcb1c004a78d60ac6c57ed423
Fredy Mendivelso <fredy.mendivelso@placetopay.com>
2018-03-06 13:51:27 -0500

    Reformat env function, now use dotenv dependency

--- src/helpers.php
+++ src/helpers.php
@@ -1,35 +1,5 @@
 <?php
 
-use FreddieGar\DeclarationApi\Exceptions\DeclarationApiException;
-
-if (!function_exists('env')) {
-    /**
-     * @param $var
-     * @param null $default
-     * @return null
-     * @throws DeclarationApiException
-     */
-    function env($var, $default = null)
-    {
-        static $env = [];
-
-        if (count($env) == 0) {
-            $configs = file('../.env');
-
-            if (!$configs) {
-                return $env[$var] = $default;
-            }
-
-            foreach ($configs as $config) {
-                list($name, $value) = explode('=', $config);
-                $env[$name] = trim($value);
-            }
-        }
-
-        return isset($env[$var]) ? $env[$var] : $default;
-    }
-}
-
 if (!function_exists('isConsole')) {
     /**
      * @return bool
